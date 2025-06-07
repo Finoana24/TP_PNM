e1c230986af63f528378b6c23235c69869d81467
Finoana RAZAFINDRAMAVO <razafindramavofinoana@gmail.com>
2025-06-07 14:34:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,3 @@
-from boisson import afficher_menu, traiter_choix
-
-def main():
-    commandes = []
-
-    while True:
-        afficher_menu()
-        saisie = input("Entrez vos choix (ex : 1 2 1) ou 4 pour quitter : ")
-        choix_liste = saisie.strip().split()
-
-        quitter = False
-
-        for choix in choix_liste:
-            boisson = traiter_choix(choix)
-
-            if boisson == "Quitter":
-                quitter = True
-                break
-            else:
-                boisson
-                commandes.append(boisson)
-                print(f"→ {boisson} ajouté à votre commande.")
-           
-           
-           
-    if commandes:
-        print("\nVoici votre commande finale :")
-        for index, item in enumerate(commandes, start=1):
-            print(f"{index}. {item}")
-    else:
-        print("\nAucune boisson commandée.")
-
-    print("Merci et à bientôt ! 👋")
-
-if __name__ == "__main__":
-    main()
+from boisson2 import gerer_choix
+print("bienvenue")
+gerer_choix()
